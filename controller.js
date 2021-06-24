e824cacca4c8382e885fde98edfd6af544fcb5e6
akhilkumarpilli <akhilkumar7947@gmail.com>
2021-06-24 12:41:25 +0530

    Add time filters to query

--- controller.js
+++ controller.js
@@ -2,7 +2,21 @@ var UnrelayedModel = require("./schema.js");
 
 const getStats = (req, res) => {
     let { query } = req
-    let filters = query.path ? { pathName: query.path } : {}
+    let filters = {}
+    if (query.path) {
+        filters["pathName"] = query.path;
+    }
+
+    if (query.start_date) {
+        filters.time = filters.time || {};
+        filters["time"]["$gte"] = new Date(query.start_date)
+    }
+
+    if (query.end_date) {
+        filters.time = filters.time || {};
+        filters["time"]["$lte"] = new Date(query.end_date)
+    }
+
     UnrelayedModel.find(filters, {}, { sort: { time: 1 } }, (err, data) => {
         if (err) {
             res.status(500).send({
